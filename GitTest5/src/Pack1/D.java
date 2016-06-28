72ddc271fbd598d6016e9cafd0e07a4530ff1e63
Suresh Nemani <chandra.nemani@gmail.com>
2016-06-28 14:19:18 +0530

    Commit branch 5 only

--- GitTest5/src/Pack1/D.java
+++ GitTest5/src/Pack1/D.java
@@ -4,6 +4,7 @@ public class D {
 	public static void main(String[] args){
 		System.out.println("D class");
 		System.out.println("Branch added in d class");
+		System.out.println("Adding on Branch 5");
 	}
 
 }
